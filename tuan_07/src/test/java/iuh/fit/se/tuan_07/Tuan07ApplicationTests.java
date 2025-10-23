7481b8f2547c586e4405fcd17d836100892905fa
PhamNguyenHuyHoang <pnhh28012002@gmail.com>
2025-10-23 14:21:37 +0700

    Add files via upload

---
+++ tuan_07/src/test/java/iuh/fit/se/tuan_07/Tuan07ApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.tuan_07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tuan07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
